3b7dc6a428c70bd6a85b0098a674f3e5e38fa0b8
unknown <payalashtikar@gmail.com>
2023-11-09 10:44:39 +0530

    server-folder-setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const dotEnv = require('dotenv')
+dotEnv.config()
+port = process.env.PORT
+
+app.get('/', async (req, res) => {
+    res.send("Hello World")
+})
+
+
+
+
+app.listen(port, (err) => {
+    if (err) throw err
+    console.log(`server running on port ${port}`)
+})
